7f6f881b9af8f87428e76e094eded881353a5d3d
mateusz <mateusz@MATI>
2016-11-11 13:58:39 +0100

    Liczenie głosów w ankiecie

--- ShadoWarp/Models/AnkietaModelView.cs
+++ ShadoWarp/Models/AnkietaModelView.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,11 @@ namespace ShadoWarp.Models
     {
         public string ans { get; private set; }
         public string komentarz { get; private set; }
+        private static int glosRok;
+        private static int glosDwa;
+        private static int glosTrzy;
+        private static int glosWiecej; 
+        public int iloscGlosow { get; private set; }
         public AnkietaModelView(string s)
         {
             switch (s)
@@ -17,24 +23,34 @@ namespace ShadoWarp.Models
                 case "rok":
                     ans = string.Format("Wybrałeś {0}!", s);
                     komentarz = "Co za nooooooooooob :P";
+                    iloscGlosow = ++glosRok;
                     break;
                 case "dwa":
                     ans = string.Format("Wybrałeś {0} lata!", s);
                     komentarz = "Tak dalej!!!";
+                    iloscGlosow = ++glosDwa;
                     break;
                 case "trzy":
                     ans = string.Format("Wybrałeś {0} lata!", s);
                     komentarz = "Zajebioza :D";
+                    iloscGlosow = ++glosTrzy;
                     break;
                 case "wiecej":
                     ans = string.Format("Wybrałeś więcej");
                     komentarz = "WOW. Jaki świrus z Ciebie, jakiś syn diabła.";
+                    iloscGlosow = ++glosWiecej;
                     break;
                 default:
                     ans = string.Format("Jaki zjeb nic nie wybrał...");
                     komentarz = string.Format("No... jakby to powiedzieć..., jakiś okrutnik :/");
                     break;
             }
+            StreamWriter sw = new StreamWriter("wyniki.txt");
+            sw.WriteLine(glosRok);
+            sw.WriteLine(glosDwa);
+            sw.WriteLine(glosTrzy);
+            sw.WriteLine(glosWiecej);
+            sw.Close();
         }
     }
 }
